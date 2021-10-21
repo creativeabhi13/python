b4e74d694636476e18e7144b3cd3dab234fd0a33
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2021-10-21 20:17:44 +0530

    py

---
+++ swap.py
@@ -0,0 +1,8 @@
+a = int(input('enter a first number->'))
+b = int(input('enter a second number->'))
+print("Before the swap a->",a,"and b->",b )
+
+temp=a
+a=b
+b=temp
+print("after the swap a->",a, "and b->",b)

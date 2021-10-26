d48a26b61e7001116a8bc59aea40daefd7b15774
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2021-10-26 23:58:51 +0530

    py

---
+++ HELLO world/operator.py
@@ -0,0 +1,12 @@
+# order of operator
+# exponential  2 ** 3
+# multiplication or division
+# addition or subtraction
+x = 10 + 3 * 2 ** 3
+print(x)
+
+y = (10 + 3) * 2 ** 3
+print(y)
+
+z = (2 + 3) * 10 - 3
+print(z)

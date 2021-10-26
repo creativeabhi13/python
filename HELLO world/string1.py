4157369dff4168a361882ccdf42aef0b5695b0b2
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2021-10-26 17:58:34 +0530

    py

---
+++ HELLO world/string1.py
@@ -0,0 +1,23 @@
+course = '''
+Hi Abhishek,
+
+here is your first email to you.
+
+Thank You,
+The support Team
+Here
+'''
+print(course)
+
+name = 'Aditya is a very good boy'
+print(name[0])
+print(name[1])
+print(name[-1])
+print(name[-2])
+
+print(name[0:5])
+print(name[1:])
+print(name[:5])
+print(name[:])
+print(name[1:-1])
+

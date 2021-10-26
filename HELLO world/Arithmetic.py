a73b5e14f9df208d341d3be8269accb5a1fb5990
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2021-10-26 23:28:27 +0530

    py

---
+++ HELLO world/Arithmetic.py
@@ -0,0 +1,22 @@
+# addition operator
+print(10+3)
+# subtraction operator
+print(10-3)
+# multiplication operator
+print(10*3)
+# division operator
+print(10/3)
+# power operator
+print(10**3)
+# remainder operator
+print(10%3)
+
+# INCREMENT OPERATOR
+a = 10
+a = a+10
+print(a)
+a += 3
+print(a)
+
+b = 10 + 3 * 2
+print(b)

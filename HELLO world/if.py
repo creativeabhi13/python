7261d2e29bf19d330bcd08cf1a1b4d1e7196999b
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2021-10-28 00:07:12 +0530

    py

---
+++ HELLO world/if.py
@@ -0,0 +1,8 @@
+is_hot =True
+if is_hot:
+       print('it is a hot day')
+       print('kindly wear sunny days clothes')
+else :
+      print('it is cold day')
+      print('kindly wear cold clothes')
+print("it's a wonderful day | enjoy your day !")

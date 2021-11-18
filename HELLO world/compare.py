1660cd62b3d353ee6c7640f0d427280b11051681
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2021-11-18 11:29:00 +0530

    py

---
+++ HELLO world/compare.py
@@ -0,0 +1,6 @@
+# > < >= <= ==
+temp=35
+if temp>35:
+    print("it's hot day")
+else:
+    print("it's a normal day")
---
+++ HELLO world/compare1.py
@@ -0,0 +1,8 @@
+name = "abhi"
+print(len(name))
+if len(name)<4:
+    print("name should be at least of 4 character")
+elif len(name) > 50:
+    print("name should be not more than 50 character")
+else:
+    print("it's a good name ")

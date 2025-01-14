d7feba38447df954abee6a5dd938aacbdce450bd
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2025-01-14 18:06:24 +0530

    chapter 1 done

---
+++ Revision/Chapter 1-variable and Data Type/questions.py
@@ -0,0 +1,33 @@
+#1. Write a program to take 2 input and print there sum
+
+# n1=int(input("Enter your first number:"))
+# n2=int(input("Enter your second number:"))
+
+# sum=n1+n2
+# print("sum is :",sum)
+
+#2. write a program to take input side of square and print there area
+
+# side=float(input("Enter your side of square: "))
+
+# area=side*side
+
+# print("Area is : " , area,side ** 2)
+
+#3. write a program to input 2 floating  point numbers  and print there average
+
+# num1=float(input("Enter your 1st number : "))
+
+# num2 =float(input("Enter your 2nd number : "))
+
+# avg =( num1+num2)/2
+
+# print("Average is: ",avg)
+
+
+#4. write a program to int input a and b print True if a is greater than or equal to b if not print false
+
+a=int(input("enter your first number: "))
+b=int(input("enter your second number: "))
+
+print(a>=b)
